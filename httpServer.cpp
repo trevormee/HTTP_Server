94e325a670fc9df79cff27ef801491f222d10274
Trevor Mee <tvm5@students.uwf.edu>
2025-01-28 19:08:44 -0600

    added signal handling and made server more verbose

--- httpServer.cpp
+++ httpServer.cpp
@@ -1,3 +1,12 @@
+/***************************************************************
+  Student Name: Trevor Mee
+  File Name: httpServer.cpp
+  Project 1
+
+  @brief Simple HTTP server that uses TCP sockets to handle 
+         GET requests
+***************************************************************/
+
 #include <stdio.h>          
 #include <stdlib.h>         
 #include <sys/socket.h>     
@@ -7,9 +16,17 @@
 #include <unistd.h>
 #include <fstream>
 #include <sstream>
+#include <csignal>
 
 const int PORT = 60001;
 
+/*
+    @brief handles client requests
+
+    @param client_fd: file descriptor for the client
+
+    @return N/A
+*/
 void handleClient(int client_fd)
 {
     char buffer[4096] = {0};
@@ -27,12 +44,14 @@ void handleClient(int client_fd)
     std::istringstream request(buffer);
     std::string method, path, version;
     request >> method >> path >> version;
+    std::cout << "Processing request: " << method << " " << path << " " << version << std::endl;
+    
     std::string response;
     
     if(method == "GET")
     {
         std::ifstream file("." + path);
-        std::cout << "path: " << path << std::endl;
+        std::cout << "Requested file path: " << path << std::endl;
 
         if (file) {
 
@@ -45,28 +64,53 @@ void handleClient(int client_fd)
             response = "HTTP/1.1 200 OK\r\nContent-Type: "+ contentType + "\r\n\r\n" + content.str();
             
         } else {
-            std::string error_message = "<html><body><h1>404 Not Found</h1></body></html>";
+            std::string error_message = "404 Not Found";
+            response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" + error_message;
+            /*
             response = "HTTP/1.1 404 Not Found\r\n";
             response += "Content-Type: text/html\r\n";
             response += "Content-Length: " + std::to_string(error_message.size()) + "\r\n";
             response += "\r\n";
             response += error_message;
+            */
         }
-
     }
     else
     {
         // handle other methods
+        std::cout << "Method not supported: " << method << std::endl;
         response = "HTTP/1.1 404 Not found\r\n\r\n";
     }
 
+    // send response 
+    std::cout << "Sending response: " << response << std::endl;
     write(client_fd, response.c_str(), response.size());
-    
 }
 
+/*
+    @brief handles graceful shutdown of the server
 
+    @param signal: signal received
+
+    @return N/A
+*/
+void signalHandler(int signal)
+{
+    std::cout << "Received signal: " << signal << ", shutting server down" << std::endl;
+    exit(signal);
+}
+
+/* @brief main function
+
+    @param N/A
+
+    @return 0 on success
+*/
 int main()
 {
+    // handle graceful shutdown (Ctrl+C)
+    signal(SIGINT, signalHandler);
+    
     int server_fd;
     
     // create the server socket
@@ -74,7 +118,9 @@ int main()
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
-    }   
+    }
+
+    std::cout << "Socket created successfully!" << std::endl;
 
     // define server address
     struct sockaddr_in server_address;
@@ -89,7 +135,7 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "Server listening on port: " << PORT << std::endl;
+    std::cout << "Server bound to port: " << PORT << std::endl;
 
     // listen for connections
     if(listen(server_fd, 5) < 0)
@@ -98,7 +144,7 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "Server waiting for connection..." << std::endl;
+    std::cout << "Server listening on port: " << PORT << std::endl;
 
     while(true)
     {
@@ -116,10 +162,12 @@ int main()
         handleClient(client_fd);
 
         // close the client socket
+        std::cout << "Closing client connection" << std::endl;
         close(client_fd);
     }
 
     // close the server socket
+    std::cout << "Server shutting down" << std::endl;
     close(server_fd);
 
     return 0;
